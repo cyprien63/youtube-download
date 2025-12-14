713263614f192a7fdd2d8e27b27a83c934fcd330
cyprien63 <p.cyprien6312@gmail.com>
2025-12-14 10:07:54 +0100

    implementation de l'instalation automatique de python

--- run.bat
+++ run.bat
@@ -4,16 +4,17 @@ CLS
 
 REM 1. VERIFICATION DE PYTHON
 ECHO [Launcher] Verification de l'installation de Python...
+
 python --version >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 GOTO INSTALL_PYTHON
+IF ERRORLEVEL 1 GOTO INSTALL_PYTHON
 GOTO DETECT_PYTHON
 
 :INSTALL_PYTHON
 ECHO [Launcher] Python n'est pas detecte.
-ECHO [Launcher] Tentative d'installation automatique (via Winget)...
+ECHO [Launcher] Tentative d'installation automatique...
 
 winget --version >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 GOTO WINGET_ERROR
+IF ERRORLEVEL 1 GOTO WINGET_ERROR
 
 ECHO [Launcher] Telechargement et installation de Python 3.12...
 winget install -e --id Python.Python.3.12 --scope user --accept-source-agreements --accept-package-agreements
@@ -24,8 +25,8 @@ TIMEOUT /T 3 >nul
 GOTO DETECT_PYTHON
 
 :WINGET_ERROR
-ECHO [ERREUR] Winget est introuvable sur ce systeme.
-ECHO [ACTION] Veuillez installer Python manuellement : https://www.python.org/downloads/
+ECHO [ERREUR] Winget est introuvable.
+ECHO [ACTION] Installez Python manuellement : https://www.python.org/downloads/
 PAUSE
 EXIT /B
 
@@ -34,9 +35,10 @@ REM 2. SELECTION DE L'EXECUTABLE PYTHON
 SET PYTHON_CMD=python
 
 %PYTHON_CMD% --version >nul 2>&1
-IF %ERRORLEVEL% EQ 0 GOTO SETUP_VENV
+IF NOT ERRORLEVEL 1 GOTO SETUP_VENV
 
 ECHO [Launcher] Recherche de l'executable Python installe...
+
 IF EXIST "%LOCALAPPDATA%\Programs\Python\Python312\python.exe" (
     SET PYTHON_CMD="%LOCALAPPDATA%\Programs\Python\Python312\python.exe"
     GOTO SETUP_VENV
@@ -62,7 +64,7 @@ IF EXIST ".venv\Scripts\python.exe" GOTO LAUNCH_APP
 ECHO [Launcher] Premier lancement detecte !
 ECHO [Launcher] Creation de l'environnement virtuel (.venv)...
 %PYTHON_CMD% -m venv .venv
-IF %ERRORLEVEL% NEQ 0 (
+IF ERRORLEVEL 1 (
     ECHO [ERREUR] Echec de la creation du venv.
     PAUSE
     EXIT /B
@@ -75,8 +77,4 @@ ECHO [Launcher] Installation des dependances...
 ECHO [Launcher] Lancement de l'application...
 ".venv\Scripts\python.exe" main.py
 
-IF %ERRORLEVEL% NEQ 0 (
-    ECHO.
-    ECHO [INFO] Programme termine.
-    PAUSE
-)
+PAUSE
